21eb31da59ddb3526fdc9cd7eabf2ecbec768030
paulinembabu <paulinembabu@gmail.com>
2017-05-05 13:10:00 +0300

    notifications

--- footer.php
+++ footer.php
@@ -57,10 +57,10 @@
     <!--  Google Maps Plugin    -->
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
 
-    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-	<script src="assets/js/light-bootstrap-dashboard.js"></script>
+    <!-- findme Bootstrap Table Core javascript and methods for Demo purpose -->
+	<script src="assets/js/findme.js"></script>
 
-	<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+	<!-- findme Table DEMO methods, don't include it in your project! -->
 	<script src="assets/js/demo.js"></script>
 
     <script>
